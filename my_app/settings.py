491aca8cba815b557505b027d661c851d524a772
Alejandro Aguirre Cardona <alaguirre@unal.edu.co>
2023-06-19 15:43:55 -0500

    Update settings.py

--- my_app/settings.py
+++ my_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s5x$!+knx0j736fq#zh3+ye!*6ymkfah%lk-=0b*mdfgy5n9%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alejandroaguirre.pythonanywhere.com']
 
 
 # Application definition
